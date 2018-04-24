7f7fb9cbfccbe3e8d9b30ae9fd2d4e65d74eeda1
Yagen <799383929@qq.com>
2018-04-24 15:13:02 +0800

    VueDemo
    VUE联系

---
+++ vueDemo2/src/App.vue
@@ -0,0 +1,84 @@
+<template>
+  <div id="app">
+    <p>{{addName}}</p>
+    <CanvasCom></CanvasCom>
+    <Demo :isNumber="number">
+        <p slot="header">一段文字23 23333</p>
+        <p>一段文字 23333</p>
+        <p slot="footer">再来一段文字</p>
+    </Demo>
+    <input type="text" v-model="number" />
+    <input type="button" value="增加" @click="addNumber">
+    <div>
+      <p v-if="isShow">{{message}}</p>
+      <input type="button" value="显示" @click="isShowMessage" />
+    </div> 
+    <ul>
+      <li v-for="item in array" @click="clickArray(item)" :key="item.id">{{item.name}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Demo from './components/Demo';
+import CanvasCom from './components/Canvas';
+export default {
+  name: 'App',
+  components: {
+    Demo,
+    CanvasCom
+  },
+  data() {
+    return {
+      number: 0,
+      message: '显示一行文字',
+      isShow: false,
+      array: [
+        {name: '第一行文字', id: 0},
+        {name: '第2行文字', id: 1},
+        {name: '第3行文字', id: 2},
+        {name: '第4行文字', id: 3},
+        {name: '第5行文字', id: 4},
+        {name: '第6行文字', id: 5},
+      ],
+      firstName: 'first'
+    }
+  },
+  /* created() {
+    alert('created');
+  },
+  mounted() {
+    alert('mounted');
+  },
+  updated() {
+    alert('updated');
+  }, */
+  computed: {
+    addName() {
+      return this.firstName = this.firstName + this.number;
+    }
+  },
+  methods: {
+    addNumber() {
+      this.number += 1;
+    },
+    isShowMessage() {
+      this.isShow = !this.isShow;
+    },
+    clickArray(item) {
+        alert(`1name${item.name}----id${item.id}`);
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vueDemo2/src/components/Demo.vue
@@ -0,0 +1,25 @@
+<template scope="props">
+    <div>
+        <p>hello world {{isNumber}}</p>
+        <slot name="header">
+            只有在没有要分发的内容时才会显示。
+        </slot>
+        <slot>
+            只有在没有要分发的内容时才会显示。
+        </slot>
+
+        <slot name="footer">
+            尾部内容
+        </slot>
+    </div>
+</template>
+
+<script>
+export default {
+  props: ['isNumber'],
+}
+</script>
+
+<style>
+
+</style>
